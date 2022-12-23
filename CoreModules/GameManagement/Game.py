2f68a70f8c6cb3a78f663c6d93d58365ac0ddd32
TheFiresword <109469684+TheFiresword@users.noreply.github.com>
2022-12-23 22:09:11 +0100

    maisons corrigées

--- CoreModules/GameManagement/Game.py
+++ CoreModules/GameManagement/Game.py
@@ -138,15 +138,40 @@ class Game:
         return status
 
     def add_multiple_buildings(self, start_pos, end_pos, version) -> bool:
+        """
         # Here we can't precisely calculate the money that will be needed to construct all the roads. we'll estimate
         # that
         estimated_counter_buildings = (abs(start_pos[0] - end_pos[0]) + 1) * (abs(start_pos[1] - end_pos[1]) + 1)
         if self.money < estimated_counter_buildings * building_dico[version].cost:
             print("Not enough money")
             return False
-        building = Building(self.map.buildings_layer, globalVar.LAYER5, version)
-        status, count = self.map.buildings_layer.add_elements_serie(start_pos, end_pos, building,
-                                                                    self.map.collisions_layers)
-        if status:
+        # building = Building(self.map.buildings_layer, globalVar.LAYER5, version)
+        """
+
+        line1, column1 = start_pos[0], start_pos[1]
+        line2, column2 = end_pos[0], end_pos[1]
+
+        if line1 >= line2:
+            vrange = range(line1, line2 - 1, -1)
+        else:
+            vrange = range(line2, line1 - 1, -1)
+
+        if column1 <= column2:
+            hrange = range(column2, column1 - 1, -1)
+        else:
+            hrange = range(column1, column2 - 1, -1)
+
+
+        # a counter that will be returned as the number of roads added
+        count = 0
+        added = False
+        # On dessine une ligne verticale de routes de la ligne de départ jusqu'à la ligne de fin
+
+        for i in vrange:
+            for j in hrange:
+                if self.add_building(i, j, version):
+                    added = True
+                    count += 1
+        if added:
             self.money -= building_dico[version].cost * count
-        return status
+        return added
