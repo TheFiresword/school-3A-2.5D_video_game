6eca3b930df4cead12bb13c41fe77963a0c4d890
AyoubS-afk <saidiayoubmohammed@gmail.com>
2023-01-03 16:04:52 +0100

    Bon Sprite pour prefet et ingenieur

--- Services/Service_Walker_Sprite_To_File.py
+++ Services/Service_Walker_Sprite_To_File.py
@@ -7,19 +7,21 @@ def walkers_to_sprite(type_walker):
     sprite_number = 0
     match type_walker:
         case "Engineer":
-            type_number = "2"
-            sprite_number = 1
+            citizen_number = "1"
+            type_number = "12"
+            sprite_number = 1137
         case "Prefect":
-            type_number = "3"
-            sprite_number = 2
+            citizen_number = "2"
+            type_number = "6"
+            sprite_number = 615
 
     up = [const.SPRITE_PATH + "Citizen" + citizen_number + "/Type" + type_number + "/Citizen0" +
-          citizen_number + "_{:05d}".format(sprite_number * 104 + 3 + k * 8) + ".png" for k in range(0, 12)]
+          citizen_number + "_{:05d}".format(sprite_number + 2 + k * 8) + ".png" for k in range(0, 12)]
     left = [const.SPRITE_PATH + "Citizen" + citizen_number + "/Type" + type_number + "/Citizen0" +
-            citizen_number + "_{:05d}".format(sprite_number * 104 + 7 + k * 8) + ".png" for k in range(0, 12)]
+            citizen_number + "_{:05d}".format(sprite_number + 6 + k * 8) + ".png" for k in range(0, 12)]
     down = [const.SPRITE_PATH + "Citizen" + citizen_number + "/Type" + type_number + "/Citizen0" +
-            citizen_number + "_{:05d}".format(sprite_number * 104 + 5 + k * 8) + ".png" for k in range(0, 12)]
+            citizen_number + "_{:05d}".format(sprite_number + 4 + k * 8) + ".png" for k in range(0, 12)]
     right = [const.SPRITE_PATH + "Citizen" + citizen_number + "/Type" + type_number + "/Citizen0" +
-             citizen_number + "_{:05d}".format(sprite_number * 104 + +1 + k * 8) + ".png" for k in range(0, 12)]
+             citizen_number + "_{:05d}".format(sprite_number + k * 8) + ".png" for k in range(0, 12)]
 
     return up, left, down, right
