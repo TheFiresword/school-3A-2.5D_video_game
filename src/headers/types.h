3c3aaa5c371700f106bead68ca8d26b0b868b579
Eloïse <eloise.bonnin@insa-cvl.fr>
2023-03-28 13:24:11 +0200

    liste des types d'actions

---
+++ src/headers/types.h
@@ -0,0 +1,13 @@
+// mettre des #define pour chaque type d'info a envoyer (attribuer un numero)
+#include <stdio.h>
+
+// comment on met les coordonnées des trucs ???
+
+#define DETRUIRE            1
+#define CONSTRUIRE_PANNEAU  2
+#define PANNEAU_TO_TANTE    3
+#define TANTE_TO_MAISON     4
+#define POSER_CHEMIN        5
+#define WALKER              6
+#define PREFECTURE          7
+#define PUIT                8
